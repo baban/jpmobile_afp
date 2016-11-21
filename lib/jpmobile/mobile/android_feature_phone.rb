664e8a73af9263921c1aad06d56171759ea96dd2
m_matsubara <m_matsubara@okwave.co.jp>
2016-11-21 19:26:51 +0900

    add: 2016's new Android Future Phone

--- lib/jpmobile/mobile/android_feature_phone.rb
+++ lib/jpmobile/mobile/android_feature_phone.rb
@@ -18,10 +18,21 @@ module Jpmobile
     class AndroidFeaturePhone < SmartPhone
       # Android Feature Phone Device User Agents
       AFP_DEVICES = [
-        /SHF31/,
-        /SHF32/,
-        /SH-06G/,
-        /F-05G/,
+        # AU : 2015, Summer Model
+        /SHF31/, # AQUOS K
+        /SHF32/, # AQUOS K
+        # AU : 2016, Winter Model
+        /KYF32/, # かんたんケータイ KYF32
+        /SHF33/, # AQUOS K
+        # DOCOMO : 2015, Summer Model
+        /SH-06G/, # AQUOSケータイ
+        /F-05G/,  # ARROWS ケータイ
+        # DOCOMO : 2015, Winter Model
+        /SH-03H/,
+        # DOCOMO : 2016, Winter Model
+        /F-02J/,  # らくらくホン F-02J
+        /SH-01J/, # AQUOS ケータイ
+        /P-01J/,  # P-smart ケータイ
       ]
 
       USER_AGENT_REGEXP = Regexp.new(AFP_DEVICES.map(&:to_s)*'|')
